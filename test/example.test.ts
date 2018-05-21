2cbbf1b9c534fffbd887851f14ada8c5c4d3ef5b
Daniel Bradley <daniel.bradley@o2.com>
2018-05-21 14:20:40 +0100

    Make example test more helpful
    - Show how to do end-to-end testing with express.

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,5 +1,14 @@
+import * as request from 'supertest';
+import { configureApp } from '../src/app';
+
 describe('a feature of the system', () => {
-  it('displays some specific behaviour', () => {
-    expect(1 + 1).toEqual(2);
+  it('displays some specific behaviour', async () => {
+    const app = configureApp();
+    const response = await request(app).get('/');
+    expect(response).toMatchObject({
+      status: 200,
+      text:
+        '<!DOCTYPE html><html><head><title>Serverless Express Typescript Starter Kit</title></head><body><h1>Serverless Express Typescript Starter Kit</h1><p>Hello world!</p></body></html>',
+    });
   });
 });
